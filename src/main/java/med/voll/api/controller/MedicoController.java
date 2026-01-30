c4df5e1a0ba88c0a9650dd805558abc6dee80981
Allisson0 <allissonhiaggo@gmail.com>
2026-01-30 19:54:09 -0300

    feat(MedicoController): paginação na listagem de médicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,10 +7,9 @@ import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/medicos")
@@ -26,9 +25,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listarMedicos(){
-        return repository.findAll().stream()
-                .map(DadosListagemMedico::new)
-                .toList();
+    public Page<DadosListagemMedico> listarMedicos(Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
