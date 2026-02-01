80f95c9b370483bcdcb555bb0beec049897aa11d
Allisson0 <allissonhiaggo@gmail.com>
2026-02-01 16:47:56 -0300

    feat(SecurityFilter): validar token no filtro e recuperar subject (login)

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,12 +13,14 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         var tokenJWT = recuperarToken(request);
-
-
+        var subject = tokenService.getSubject(tokenJWT);
 
         //Dispara o próximo filtro (ou envia a requisição para o Controller)
         filterChain.doFilter(request, response);
