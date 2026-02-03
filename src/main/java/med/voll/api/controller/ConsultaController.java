0670903e67d048aec4556bab8824a6fd1f06b35c
Allisson0 <allissonhiaggo@gmail.com>
2026-02-03 20:38:04 -0300

    feat: classe consulta e detalhamento de consulta

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,6 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +19,7 @@ public class ConsultaController {
 
         System.out.println(dados);
 
-        return ResponseEntity.ok(new DadosDetalhamentoConsulta(cadastro));
+        return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null , null, null));
 
     }
 
