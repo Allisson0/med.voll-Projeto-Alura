c760cef302f9bedb44f8713d004091e7430d09e0
Allisson0 <allissonhiaggo@gmail.com>
2026-02-02 21:41:29 -0300

    add(Paciente): criando a classe Paciente

---
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -0,0 +1,29 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.domain.endereco.Endereco;
+
+@Entity(name = "Paciente")
+@Table(name = "pacientes")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String telefone;
+    private String CPF;
+
+    @Embedded
+    private Endereco endereco;
+
+}
