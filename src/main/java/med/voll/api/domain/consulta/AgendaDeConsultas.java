673bd39c911cb3b637fafbd1e6a22b3c30d27b90
Allisson0 <allissonhiaggo@gmail.com>
2026-02-03 22:03:40 -0300

    feat: retorna médico com base em id, ou aleatório se id = null

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,6 +1,7 @@
 package med.voll.api.domain.consulta;
 
 import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,18 +21,28 @@ public class AgendaDeConsultas {
 
     public void agendar(DadosAgendamentoConsulta dados){
 
+        // Verifica se os ids de médico e paciente, existem no
+        // banco de dados
         if (!pacienteRepository.existsById(dados.idPaciente())){
             throw new ValidacaoException("Id do paciente informado não existe!");
         }
 
+        // Para verificar o id do médico, é necessário que tenha vindo
+        // algum id no corpo da requisição.
         if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())){
             throw new ValidacaoException("Id do médico informado não existe!");
         }
 
         // Recuperar os objetos paciente e médico para inserção no
         // objeto consulta a ser criado.
-        var paciente = pacienteRepository.findById(dados.idPaciente()).get();
-        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
+
+        // Retorna um objeto do tipo Medico de duas formas:
+        // 1 - haja um idMedico válido presente em dados, busca o médico
+        // correspondente.
+        // 2 - nenhum idMedico no corpo (null), retorna um médico aleatório
+        // do banco de dados.
+        var medico = escolherMedico(dados);
 
         // Construtor com todos os argumentos vindo de Lombok
         // Passamos o ID como nulo, pois o JPA irá gerá-lo no
@@ -39,6 +50,18 @@ public class AgendaDeConsultas {
         var consulta = new Consulta(null, medico, paciente, dados.data());
 
         consultaRepository.save(consulta);
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        if (dados.idMedico() !=  null){
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+
+        if (dados.especialidade() == null){
+            throw new ValidacaoException("Especialidade é obrigatória quando o médico não for escolhido!");
+        }
+
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
     }
 
 }
